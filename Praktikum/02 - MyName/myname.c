b25e7774ad100578c0d5729a725b5ddab739b8c3
Benjamin Ewert <be9156@thi.de>
2013-10-25 13:06:19 +0200

    Übung 02 hinzugefügt

---
+++ Praktikum/02 - MyName/myname.c
@@ -0,0 +1,17 @@
+/* Übung 02 - MyName
+
+
+Es werden zwei Zeilen Code ausgegeben
+*/
+
+int main(){
+
+	char name[] = "Benjamin Ewert";
+	int day = 15;
+	int month = 8;
+	int year = 1985;
+
+	printf("Mein Name ist %s\n", name);
+	printf("Mein Geburtsdatum ist der %02d.%02d.%04d.\n", day, month, year);
+
+}
