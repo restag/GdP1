dbfd86a3f8cf7142980e86aeb496f9bc47f31f24
Benjamin Ewert <excelite@me.com>
2013-11-17 19:54:45 +0100

    finished excercise 05 trimString for Tutorium

--- Tutorium/05 - trimString/trimString.c
+++ Tutorium/05 - trimString/trimString.c
@@ -3,29 +3,35 @@ Diese Übung beinhaltet das trimmen von Strings und das Erstellen von Funktionen
 */
 
 
+#include <ctype.h>
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 
-char* trim(char[] t){
+char* trim(char* t);
+
+int main(void) {
+    char test[] = "Das hier \n ist der \n Teststring";
+
+    printf("%s\n", trim(test));
+    return 0;
+}
+
+char* trim(char* t){
 
 	int i;
+    int k = 0;
 
 	for (i = 0; t[i] != '\0'; i++) {
 
-		if (isblank(t[i]) || t[i] == '\n') {
+		if (!(isblank(t[i]) || t[i] == '\n')) {
 
+            t[k] = t[i];
+            k++;
 
-		}		
-
+		}
 	}
+    
+    t[k] = '\0';
 
-	return t;
-}
-
-int main (void){
-	char mySting [] = "Dies ist der verdammte String der getrimmt werden soll.";
-
-	
-	printf("%s", trim(myString));
-	return 0;
+    return t;
 }
