a4f876f755c813584f7ba8423e4b85981e116083
Benjamin Ewert <excelite@me.com>
2013-12-13 00:13:29 +0100

    updated documentation, cleaned up worm_model in Worm070v2

--- Praktikum/09 - Worm070v2/message_model.c
+++ Praktikum/09 - Worm070v2/message_model.c
@@ -1,4 +1,18 @@
-/* put a description in here 
+/* A simple variant of the game Snake
+ * Used for teaching in classes
+ *
+ * This version is an alternativ version of the given code,
+ * following a more strict approach of separation
+ *
+ *
+ * Author:
+ * Benjamin Ewert
+ * (c) 2013
+ *
+ * Author of original version:
+ * Franz Regensburger
+ * Ingolstadt University of Applied Sciences
+ * (C) 2011
  *
  */
 
@@ -9,7 +23,7 @@
 // put framework header includes below here
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+
 // put custom header includes below here
 #include "message_model.h"
 
--- Praktikum/09 - Worm070v2/message_model.h
+++ Praktikum/09 - Worm070v2/message_model.h
@@ -1,4 +1,18 @@
-/* put a description in here 
+/* A simple variant of the game Snake
+ * Used for teaching in classes
+ *
+ * This version is an alternativ version of the given code,
+ * following a more strict approach of separation
+ *
+ *
+ * Author:
+ * Benjamin Ewert
+ * (c) 2013
+ *
+ * Author of original version:
+ * Franz Regensburger
+ * Ingolstadt University of Applied Sciences
+ * (C) 2011
  *
  */
 
