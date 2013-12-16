a1fda4ba37dd17cd20ac3a8fbeb40964b761821a
Benjamin Ewert <excelite@me.com>
2013-12-16 23:15:16 +0100

    initial commit of Worm080

---
+++ Praktikum/10 - Worm080/worm.h
@@ -0,0 +1,63 @@
+/* A simple variant of the game Snake
+ * Used for teaching in classes
+ *
+ * This version is an alternativ version of the given code,
+ * following a more strict approach of separation
+ *
+ *
+ * Author:
+ * Benjamin Ewert
+ * (c) 2013
+ *
+ * Author of original version:
+ * Franz Regensburger
+ * Ingolstadt University of Applied Sciences
+ * (C) 2011
+ *
+ */
+
+#ifndef _WORM_H
+#define _WORM_H
+//*********************************************************
+//* header includes 
+//*********************************************************
+// put framework header includes below here
+
+// put custom header includes below here
+
+
+//*********************************************************
+//* defines
+//*********************************************************
+// put defines below here
+
+
+//*********************************************************
+//* enums
+//*********************************************************
+// put enums below here
+
+
+//*********************************************************
+//* enum type definitions
+//*********************************************************
+// put typedefs for enums here
+
+
+//*********************************************************
+//* structs
+//*********************************************************
+// put structs below here
+
+
+//*********************************************************
+//* struct type definitions
+//*********************************************************
+// put struct typedefs below here
+
+
+//*********************************************************
+//* function prototypes
+//*********************************************************
+// put prototypes below here
+#endif  // #define _WORM_H
